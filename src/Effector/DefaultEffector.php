5953db4fa9dd48dfc78d3dcfb9b04b4ff61dcfda
Jon Lee <techlee@qq.com>
2021-12-17 14:48:11 +0800

    perf: Optimize DefaultEffector to improve performance

--- src/Effector/DefaultEffector.php
+++ src/Effector/DefaultEffector.php
@@ -17,10 +17,10 @@ class DefaultEffector extends Effector
      * Merges all matching results collected by the enforcer into a single decision.
      *
      * @param string $expr
-     * @param array  $effects
-     * @param array  $matches
-     * @param int    $policyIndex
-     * @param int    $policyLength
+     * @param array $effects
+     * @param array $matches
+     * @param int $policyIndex
+     * @param int $policyLength
      *
      * @return array
      *
@@ -31,81 +31,79 @@ class DefaultEffector extends Effector
         $result = Effector::INDETERMINATE;
         $explainIndex = -1;
 
-        // short-circuit some effects in the middle
-        if ($expr != 'priority(p_eft) || deny' && $expr != 'subjectPriority(p_eft) || deny') {
-            if ($policyIndex < $policyLength - 1) {
-                // choose not to short-circuit
-                return [$result, $explainIndex];
-            }
-        }
-
-        // merge all effects at last
-        if ($expr == 'some(where (p_eft == allow))') {
-            $result = Effector::INDETERMINATE;
-            foreach ($effects as $i => $eft) {
-                if ($matches[$i] == 0) {
-                    continue;
-                }
-
-                if ($eft == Effector::ALLOW) {
-                    $result = Effector::ALLOW;
-                    $explainIndex = $i;
+        switch ($expr) {
+            case "some(where (p_eft == allow))":
+                if ($matches[$policyIndex] == 0) {
                     break;
                 }
-            }
-        } elseif ($expr == '!some(where (p_eft == deny))') {
-            // if no deny rules are matched, then allow
-            $result = Effector::ALLOW;
-            foreach ($effects as $i => $eft) {
-                if ($matches[$i] == 0) {
-                    continue;
+                // only check the current policyIndex
+                if ($effects[$policyIndex] === Effector::ALLOW) {
+                    $result = Effector::ALLOW;
+                    $explainIndex = $policyIndex;
+                    break;
                 }
-
-                if ($eft == Effector::DENY) {
+                break;
+            case "!some(where (p_eft == deny))":
+                // only check the current policyIndex
+                if ($matches[$policyIndex] != 0 && $effects[$policyIndex] === Effector::DENY) {
                     $result = Effector::DENY;
-                    $explainIndex = $i;
+                    $explainIndex = $policyIndex;
                     break;
                 }
-            }
-        } elseif ($expr == 'some(where (p_eft == allow)) && !some(where (p_eft == deny))') {
-            $result = Effector::INDETERMINATE;
-            foreach ($effects as $i => $eft) {
-                if ($matches[$i] == 0) {
-                    continue;
-                }
-    
-                if ($eft == Effector::ALLOW) {
-                    // set hit rule to first matched allow rule, maybe overridden by the deny part
-                    if ($result == Effector::INDETERMINATE) {
-                        $explainIndex = $i;
-                    }
+                // if no deny rules are matched  at last, then allow
+                if ($policyIndex == $policyLength - 1) {
                     $result = Effector::ALLOW;
-                } elseif ($eft == Effector::DENY) {
+                }
+                break;
+            case "some(where (p_eft == allow)) && !some(where (p_eft == deny))":
+                // short-circuit if matched deny rule
+                if ($matches[$policyIndex] != 0 && $effects[$policyIndex] === Effector::DENY) {
                     $result = Effector::DENY;
                     // set hit rule to the (first) matched deny rule
-                    $explainIndex = $i;
+                    $explainIndex = $policyIndex;
                     break;
                 }
-            }
-        } elseif ($expr == 'priority(p_eft) || deny' || $expr == 'subjectPriority(p_eft) || deny') {
-            $result = Effector::INDETERMINATE;
-            foreach ($effects as $i => $eft) {
-                if ($matches[$i] == 0) {
-                    continue;
+
+                // short-circuit some effects in the middle
+                if ($policyIndex < $policyLength - 1) {
+                    // choose not to short-circuit
+                    return [$result, $explainIndex];
                 }
-    
-                if ($eft != Effector::INDETERMINATE) {
-                    if ($eft == Effector::ALLOW) {
-                        $result = Effector::ALLOW;
-                    } else {
-                        $result = Effector::DENY;
+                // merge all effects at last
+                foreach ($effects as $i => $eft) {
+                    if ($matches[$i] == 0) {
+                        continue;
                     }
-                    $explainIndex = $i;
-                    break;
+
+                    if ($eft === Effector::ALLOW) {
+                        $result = Effector::ALLOW;
+                        // set hit rule to first matched allow rule
+                        $explainIndex = $i;
+                        break;
+                    }
                 }
-            }
-        } else {
-            throw new CasbinException('unsupported effect');
+                break;
+            case "priority(p_eft) || deny":
+            case "subjectPriority(p_eft) || deny":
+                // reverse merge, short-circuit may be earlier
+                for ($i = count($effects) - 1; $i >= 0; $i--) {
+                    if ($matches[$i] == 0) {
+                        continue;
+                    }
+
+                    if ($effects[$i] != Effector::INDETERMINATE) {
+                        if ($effects[$i] === Effector::ALLOW) {
+                            $result = Effector::ALLOW;
+                        } else {
+                            $result = Effector::DENY;
+                        }
+                        $explainIndex = $i;
+                        break;
+                    }
+                }
+                break;
+            default:
+                throw new CasbinException('unsupported effect');
         }
 
         return [$result, $explainIndex];
