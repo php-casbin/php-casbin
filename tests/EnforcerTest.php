9f371f1ce43844d5da4a3e5de0d3506a03a8b9a5
Stephane COUTURAUD <stephane.couturaud@edenred.com>
2023-11-20 17:31:49 +0100

    fix: fixed rbac with domain and pattern (#147) added yield to unit test

--- tests/EnforcerTest.php
+++ tests/EnforcerTest.php
@@ -430,46 +430,56 @@ class EnforcerTest extends TestCase
         $this->assertTrue($e->enforce('anyone', 'data3', 'read'));
     }
 
-    public function testRbacWithDomain()
+    /**
+     * Data provider for testRbacWithDomain.
+     */
+    public function domainDataProvider(): \Generator
     {
-        $e = new Enforcer(
-            $this->modelAndPolicyPath . '/rbac_with_domain_pattern_model_and_keymatch_model.conf',
-            $this->modelAndPolicyPath . '/rbac_with_domain_pattern_model_and_keymatch_policy.csv'
+        yield 'alice in domain1 data1 read' => ['alice', 'domain1', 'data1', 'read', true];
+        yield 'alice in domain1 data1 write' => ['alice', 'domain1', 'data1', 'write', true];
+        yield 'alice in domain1 data2 read' => ['alice', 'domain1', 'data2', 'read', true];
+        yield 'alice in domain1 data2 write' => ['alice', 'domain1', 'data2', 'write', true];
+        yield 'bob in domain1 data2 write' => ['bob', 'domain1', 'data2', 'write', false];
+        yield 'alice in domain2 data1 read' => ['alice', 'domain2', 'data1', 'read', true];
+        yield 'alice in domain2 data1 write' => ['alice', 'domain2', 'data1', 'write', false];
+        yield 'alice in domain2 data2 read' => ['alice', 'domain2', 'data2', 'read', true];
+        yield 'alice in domain2 data2 write' => ['alice', 'domain2', 'data2', 'write', false];
+        yield 'alice in domain3 data1 read' => ['alice', 'domain3', 'data1', 'read', false];
+        yield 'alice in domain3 data1 write' => ['alice', 'domain3', 'data1', 'write', false];
+        yield 'alice in domain3 data2 read' => ['alice', 'domain3', 'data2', 'read', false];
+        yield 'alice in domain3 data2 write' => ['alice', 'domain3', 'data2', 'write', false];
+        yield 'badr in domain4 data1 write' => ['badr', 'domain4', 'data1', 'write', false];
+        yield 'badr in domain4 data1 read' => ['badr', 'domain4', 'data1', 'read', true];
+        yield 'badr in domain4 data2 read' => ['badr', 'domain4', 'data2', 'read', true];
+        yield 'badr in domain4 data2 write' => ['badr', 'domain4', 'data2', 'write', true];
+        yield 'stef in domain1 data1 read' => ['stef', 'domain1', 'data1', 'read', true];
+        yield 'stef in domain1 data1 write' => ['stef', 'domain1', 'data1', 'write', true];
+        yield 'stef in domain1 data2 read' => ['stef', 'domain1', 'data2', 'read', true];
+        yield 'stef in domain1 data2 write' => ['stef', 'domain1', 'data2', 'write', true];
+        yield 'stef in domain5 data3 read' => ['stef', 'domain5', 'data3', 'read', false];
+        yield 'ben in domain5 data3 read' => ['ben', 'domain5', 'data3', 'read', false];
+        yield 'leo in domain5 data3 read' => ['leo', 'domain5', 'data3', 'read', true];
+        yield 'leo in domain1 data3 read' => ['leo', 'domain1', 'data3', 'read', false];
+    }
+
+    /**
+     * @dataProvider domainDataProvider
+     */
+    public function testRbacWithDomain($user, $domain, $resource, $action, $expected)
+    {
+        $enforcer = new Enforcer(
+            $this->modelAndPolicyPath.'/rbac_with_domain_pattern_model_and_keymatch_model.conf',
+            $this->modelAndPolicyPath.'/rbac_with_domain_pattern_model_and_keymatch_policy.csv'
         );
 
-        $e->addNamedDomainMatchingFunc('g', 'keyMatch', function (string $key1, string $key2) {
-            return BuiltinOperations::keyMatch($key1, $key2);
-        });
+        $enforcer->addNamedDomainMatchingFunc(
+            'g',
+            'keyMatch',
+            function (string $keyOne, string $keyTwo) {
+                return BuiltinOperations::keyMatch($keyOne, $keyTwo);
+            }
+        );
 
-        $this->assertTrue($e->enforce('alice', 'domain1', 'data1', 'read'));
-        $this->assertTrue($e->enforce('alice', 'domain1', 'data1', 'write'));
-        $this->assertTrue($e->enforce('alice', 'domain1', 'data2', 'read'));
-        $this->assertTrue($e->enforce('alice', 'domain1', 'data2', 'write'));
-
-        $this->assertFalse($e->enforce('bob', 'domain1', 'data2', 'write'));
-
-        $this->assertTrue($e->enforce('alice',  'domain2', 'data1', 'read'));
-        $this->assertFalse($e->enforce('alice', 'domain2', 'data1', 'write'));
-        $this->assertTrue($e->enforce('alice',  'domain2', 'data2', 'read'));
-        $this->assertFalse($e->enforce('alice', 'domain2', 'data2', 'write'));
-
-        $this->assertFalse($e->enforce('alice', 'domain3', 'data1', 'read'));
-        $this->assertFalse($e->enforce('alice', 'domain3', 'data1', 'write'));
-        $this->assertFalse($e->enforce('alice', 'domain3', 'data2', 'read'));
-        $this->assertFalse($e->enforce('alice', 'domain3', 'data2', 'write'));
-
-        $this->assertFalse($e->enforce('badr', 'domain4', 'data1', 'write'));
-        $this->assertTrue($e->enforce('badr', 'domain4', 'data1', 'read'));
-        $this->assertTrue($e->enforce('badr', 'domain4', 'data2', 'read'));
-        $this->assertTrue($e->enforce('badr', 'domain4', 'data2', 'write'));
-
-        $this->assertTrue($e->enforce('stef', 'domain1', 'data1', 'read'));
-        $this->assertTrue($e->enforce('stef', 'domain1', 'data1', 'write'));
-        $this->assertTrue($e->enforce('stef', 'domain1', 'data2', 'read'));
-        $this->assertTrue($e->enforce('stef', 'domain1', 'data2', 'write'));
-
-        $this->assertFalse($e->enforce('stef', 'domain5', 'data3', 'read'));
-        $this->assertFalse($e->enforce('ben', 'domain5', 'data3', 'read'));
-        $this->assertTrue($e->enforce('leo', 'domain5', 'data3', 'read'));
+        $this->assertSame($expected, $enforcer->enforce($user, $domain, $resource, $action));
     }
 }
